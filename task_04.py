951be589beeaf840b69d5320dab439e181de2784
Biencis Rubiera <biencis@gmail.com>
2016-09-23 16:43:49 +0000

    Exchanged values using .format()

--- task_04.py
+++ task_04.py
@@ -6,3 +6,8 @@ NEWS = 'Hi {friend}! I have {0} news! I won the raffle with number {1}!'
 FNAME = 'Pat'
 NTYPE = '*amazing*'
 RNUM = 42
+
+EMAIL = ('Hi {}! I have {} news! I won the raffle with number {:06d}!'.format
+        (FNAME, NTYPE, RNUM))
+
+print EMAIL
